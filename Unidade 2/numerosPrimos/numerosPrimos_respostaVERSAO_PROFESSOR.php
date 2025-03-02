b3657affa0e1cc114de41e13820f684d86e28c16
Jader Lemes <62562208+jaderloki@users.noreply.github.com>
2025-03-02 20:41:57 -0300

    [ADD] adicionar ZIP e versão do professor para referência

---
+++ Unidade 2/numerosPrimos/numerosPrimos_respostaVERSAO_PROFESSOR.php
@@ -0,0 +1,18 @@
+<?php
+/**
+*   ESSA É A VERSÃO DO PROFESSOR
+**/
+$i= 1;
+while ($i <= 100) {
+    $primo= 1;
+    for ($n= 2; $n < $i; $n++) {
+        if ($i % $n == 0) {
+            $primo= 0;
+            break;
+        }
+    }
+    if ($primo == 1)
+        echo "$i ";
+        $i++;
+}
+?>
